8849d44b5ee65b3ff11bc178bbe0296ecaa9e12a
jinyufan <jyf19901015@126.com>
2017-11-06 15:27:36 +0800

    nothing serious

--- src/pers/rogerjin/LeetCode/Solution/Array/MaximumSwap.java
+++ src/pers/rogerjin/LeetCode/Solution/Array/MaximumSwap.java
@@ -5,32 +5,43 @@ import java.util.ArrayList;
 public class MaximumSwap {
     public int maximumSwap(int num) {
         ArrayList<Integer> array = new ArrayList<Integer>();
-        int tmp = num;
-        int currentDigit = 0;
-        int currentIndex = 0;
-        int swapIndex = 0;
-        do {
-            currentDigit = tmp % 10;
-            array.add(currentDigit);
-            if (array.get(swapIndex) < currentDigit) {
-                swapIndex = currentIndex;
-            }
-            ++currentIndex;
-            tmp = tmp / 10;
-        } while (tmp > 0);
-        //not need swap
-        if (swapIndex == array.size() - 1) {
+        String temp = String.valueOf(num);
+        int[] digits = new int[temp.length()];
+        for (int i = 0; i < temp.length(); i++) {
+            digits[i] = temp.charAt(i) - '0';
+        }
+        if (digits.length < 2) {
             return num;
         } else {
-            int swapDigit = array.get(swapIndex);
-            array.set(swapIndex, array.get(array.size() - 1));
-            array.set(array.size() - 1, swapDigit);
-            int returnNum = 0;
-            for (int i = array.size() - 1; i >= 0; i--) {
-                returnNum = returnNum * 10 + array.get(i);
+            int swapIndex = 0;
+            for (int i = 0; i < temp.length() - 1; i++) {
+                if (digits[i] < digits[i + 1]) {
+                    swapIndex = i + 1;
+                    break;
+                }
             }
-            return returnNum;
+            for (int j = 0; j < swapIndex; j++) {
+                if (digits[j] < digits[swapIndex]) {
+                    int swapDigit = 0;
+                    swapDigit = digits[j];
+                    digits[j] = digits[swapIndex];
+                    digits[swapIndex] = swapDigit;
+                    break;
+                }
+            }
+            int num1 = 0;
+            for (int i = 0; i < digits.length; i++) {
+                num1 = num1 * 10 + digits[i];
+            }
+            return num1;
         }
 
+    }
+
+    public static void main(String[] args) {
+        MaximumSwap ms = new MaximumSwap();
+        int num = 2736;
+        int ret = ms.maximumSwap(num);
+        System.out.println(ret);
     }
 }
